65f031841d375a91cc4a579e8b354e74ab0636c2
Ritesh Pathania <riteshpathaniawins@gmail.com>
2024-12-21 21:49:41 +0530

    AndroidDrievr changes

--- src/test/java/com/qa/test/AndroidBasic.java
+++ src/test/java/com/qa/test/AndroidBasic.java
@@ -16,12 +16,15 @@ public class AndroidBasic {
 	@Test
 	public void AppiumTest() throws MalformedURLException {
 
+		// Start to Appium server 
 		AppiumDriverLocalService service = new AppiumServiceBuilder()
 				.withAppiumJS(new File("//usr//local//lib//node_modules//appium//build//lib//main.js"))
 				.withIPAddress("127.0.0.1").usingPort(4723).build();
 		service.start();
 		// AndroidDriver , IOSDriver
 		// Appium code -> appium server -> Mobile
+		
+		System.out.println("Server start sucessfully");
 
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("Pixel 8"); // emulator
@@ -29,6 +32,7 @@ public class AndroidBasic {
   
 
 		AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+		
 
 		driver.quit();
 		service.stop();
